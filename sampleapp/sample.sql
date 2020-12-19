a26d3cafd3f1f2e33993944517001d598219bfd7
Mihiri0303 <93081@fhss.sjp.ac.lk>
2020-12-19 22:14:57 +0530

    database updated

--- sampleapp/sample.sql
+++ sampleapp/sample.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 19, 2020 at 07:44 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.34
+-- Generation Time: Dec 19, 2020 at 05:42 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -38,6 +38,10 @@ CREATE TABLE `admin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- RELATIONSHIPS FOR TABLE `admin`:
+--
+
+--
 -- Dumping data for table `admin`
 --
 
@@ -45,9 +49,9 @@ INSERT INTO `admin` (`id`, `addname`, `addnic`, `addemail`, `addpassword`, `adda
 (1, ' ytffgg', ' 345345', '123', '123', ' yttyt', ' 34345'),
 (2, ' wasantha', ' 6878678', ' wasantha@gmail', 'wasantha123 ', ' negampaha', ' 0716662908'),
 (3, 'sdf', '234', '1233', '1233', '', '234'),
-(7, 'WW', 'DD', 'DD', 'AS', 'AS', 'WEWE'),
-(9, 'R', 'FFffffffffff', 'F', 'F', 'F', 'F'),
-(11, 'A.D.Peiris', '1234567V', 'aqw12@gmail.com', 'uio123', 'kaluthara,sri lanka', '0987667789');
+(9, 'Roshini', 'FFffffffffff', 'F', 'F', 'F', 'F'),
+(11, 'A.D.Peiris', '1234567V', 'aqw12@gmail.com', 'uio123', 'kaluthara,sri lanka', '0987667789'),
+(12, 'kumara', '46764V', '@com', '123qwe', 'Galle', '09876534');
 
 -- --------------------------------------------------------
 
@@ -61,14 +65,18 @@ CREATE TABLE `course` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- RELATIONSHIPS FOR TABLE `course`:
+--
+
+--
 -- Dumping data for table `course`
 --
 
 INSERT INTO `course` (`id`, `course_name`) VALUES
-(1, 'ICT2'),
-(2, 'MATHS'),
+(1, 'ICT'),
+(2, 'MATHSsd'),
 (3, 'Python'),
-(25, 'Java');
+(4, 'sdf');
 
 -- --------------------------------------------------------
 
@@ -82,6 +90,12 @@ CREATE TABLE `exam` (
   `course_name` varchar(20) NOT NULL,
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- RELATIONSHIPS FOR TABLE `exam`:
+--   `course_id`
+--       `course` -> `id`
+--
 
 --
 -- Dumping data for table `exam`
@@ -104,6 +118,10 @@ CREATE TABLE `spnote` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- RELATIONSHIPS FOR TABLE `spnote`:
+--
+
+--
 -- Dumping data for table `spnote`
 --
 
@@ -112,7 +130,8 @@ INSERT INTO `spnote` (`No`, `Notice`) VALUES
 (2, ' check 2'),
 (3, ' a'),
 (4, '123234'),
-(5, 'No anyone');
+(5, 'No anyone'),
+(6, 'qwe');
 
 -- --------------------------------------------------------
 
@@ -134,24 +153,29 @@ CREATE TABLE `student` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- RELATIONSHIPS FOR TABLE `student`:
+--   `course_id`
+--       `course` -> `id`
+--
+
+--
 -- Dumping data for table `student`
 --
 
 INSERT INTO `student` (`id`, `arnumber`, `name`, `course_id`, `stemail`, `stpassword`, `tpnumber`, `stadd`, `stdob`, `gender`) VALUES
-(1, 92946, 'kavi', 0, NULL, NULL, 123456789, NULL, NULL, 'M'),
-(2, 1234, 'ksd', 0, NULL, NULL, 11223344, NULL, NULL, 'M'),
-(3, 92914, 'kavudu', 0, NULL, NULL, 712698828, NULL, NULL, 'M'),
-(4, 76544, 'khgfdh', 0, NULL, NULL, 1233445, NULL, NULL, 'M'),
-(5, 0, '', 0, '1AA', '2BB', 0, NULL, NULL, ''),
+(1, 92946, 'kavi', 1, NULL, NULL, 123456789, NULL, NULL, 'M'),
+(2, 1234, 'ksd', 1, NULL, NULL, 11223344, NULL, NULL, 'M'),
+(3, 92914, 'kavudu', 1, NULL, NULL, 712698828, NULL, NULL, 'M'),
+(4, 76544, 'khgfdh', 1, NULL, NULL, 1233445, NULL, NULL, 'M'),
+(5, 0, '', 1, '1AA', '2BB', 0, NULL, NULL, ''),
 (6, 6677, 'KKSS', 1, NULL, NULL, 9900, NULL, NULL, 'F'),
-(8, 923333, 'dilip', 0, '123', ' dilip123', 7156789, 'makiligasveva', '1998-04-23', 'M'),
-(9, 92914, 'hansi', 2, ' zvczc', ' zxcxzc', 234, ' sdfdsf', '1998-05-10', 'F'),
+(9, 92914, 'hansi', 1, ' zvczc', ' zxcxzc', 234, ' sdfdsf', '1998-05-10', 'F'),
 (21, 92914, 'hansi', 1, ' zvczc', ' zxcxzc', 234, ' sdfdsf', '1998-05-10', 'F'),
-(22, 92914, 'hansi', 1, ' zvczc', ' zxcxzc', 345, ' sdfdsf', '1998-05-10', 'F'),
-(23, 1554, '2626', 0, 'sdfsdf', '23r32r', 23234, 'sdfsf', '1998-03-03', 'm'),
-(26, 234, 'dilip', 0, '123', ' dilip123', 7156789, 'makiligasveva', '1998-04-23', 'M'),
-(27, 82934, 'kjkljlkj', 2, 'kjjljkj', '899899', 89898989, '89898', '1982-02-02', 'm'),
-(28, 56981, 'A.S.Perera', 2, 'abc123@gmail.com', 'Abc123', 98123454, 'galle,colombo', '1987-09-02', 'f');
+(23, 1554, '2626', 1, 'sdfsdf', '23r32r', 23234, 'sdfsf', '1998-03-03', 'm'),
+(26, 234, 'dilip', 3, '123', ' dilip123', 7156789, 'makiligasveva', '1998-04-23', 'M'),
+(27, 82934, 'kjkljlkj', 1, 'com', '899899', 89898989, '89898', '1982-02-02', 'm'),
+(28, 56981, 'A.S.Perera', 1, 'abc123@gmail.com', 'Abc123', 98123454, 'galle,colombo', '1987-09-02', 'f'),
+(29, 90900, 'Kumara Silva', 1, 'kumara@gmail.com', 'kumara12', 1231231234, 'Gampaha', '2000-08-15', 'm');
 
 --
 -- Indexes for dumped tables
@@ -173,7 +197,8 @@ ALTER TABLE `course`
 -- Indexes for table `exam`
 --
 ALTER TABLE `exam`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `exam_fk` (`course_id`);
 
 --
 -- Indexes for table `spnote`
@@ -185,7 +210,8 @@ ALTER TABLE `spnote`
 -- Indexes for table `student`
 --
 ALTER TABLE `student`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `course` (`course_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -195,25 +221,47 @@ ALTER TABLE `student`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `course`
+--
+ALTER TABLE `course`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `exam`
 --
 ALTER TABLE `exam`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `spnote`
 --
 ALTER TABLE `spnote`
-  MODIFY `No` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `No` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `exam`
+--
+ALTER TABLE `exam`
+  ADD CONSTRAINT `exam_fk` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Constraints for table `student`
+--
+ALTER TABLE `student`
+  ADD CONSTRAINT `course` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
