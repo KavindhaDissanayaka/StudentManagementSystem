e3b3b4c2a39be35cf7bcbedf4d4944dd453b3e83
ShashiniDileka <92947@fhss.sjp.ac.lk>
2020-12-19 12:15:56 +0530

    Update sample.sql

--- sampleapp/sample.sql
+++ sampleapp/sample.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 17, 2020 at 05:39 AM
--- Server version: 10.4.13-MariaDB
--- PHP Version: 7.4.8
+-- Generation Time: Dec 19, 2020 at 07:44 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -44,7 +44,10 @@ CREATE TABLE `admin` (
 INSERT INTO `admin` (`id`, `addname`, `addnic`, `addemail`, `addpassword`, `addadd`, `addtell`) VALUES
 (1, ' ytffgg', ' 345345', '123', '123', ' yttyt', ' 34345'),
 (2, ' wasantha', ' 6878678', ' wasantha@gmail', 'wasantha123 ', ' negampaha', ' 0716662908'),
-(3, 'sdf', '234', '1233', '1233', '', '234');
+(3, 'sdf', '234', '1233', '1233', '', '234'),
+(7, 'WW', 'DD', 'DD', 'AS', 'AS', 'WEWE'),
+(9, 'R', 'FFffffffffff', 'F', 'F', 'F', 'F'),
+(11, 'A.D.Peiris', '1234567V', 'aqw12@gmail.com', 'uio123', 'kaluthara,sri lanka', '0987667789');
 
 -- --------------------------------------------------------
 
@@ -64,7 +67,30 @@ CREATE TABLE `course` (
 INSERT INTO `course` (`id`, `course_name`) VALUES
 (1, 'ICT2'),
 (2, 'MATHS'),
-(3, 'sdfdassdf');
+(3, 'Python'),
+(25, 'Java');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam`
+--
+
+CREATE TABLE `exam` (
+  `id` int(10) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `course_name` varchar(20) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `exam`
+--
+
+INSERT INTO `exam` (`id`, `course_id`, `course_name`, `date`) VALUES
+(1, 2, 'MATHS', '2020-08-04'),
+(2, 2, 'MATHS', '2020-08-06'),
+(3, 1, 'ICT2', '2020-10-10');
 
 -- --------------------------------------------------------
 
@@ -85,7 +111,8 @@ INSERT INTO `spnote` (`No`, `Notice`) VALUES
 (1, ' check'),
 (2, ' check 2'),
 (3, ' a'),
-(4, '123234');
+(4, '123234'),
+(5, 'No anyone');
 
 -- --------------------------------------------------------
 
@@ -120,11 +147,11 @@ INSERT INTO `student` (`id`, `arnumber`, `name`, `course_id`, `stemail`, `stpass
 (8, 923333, 'dilip', 0, '123', ' dilip123', 7156789, 'makiligasveva', '1998-04-23', 'M'),
 (9, 92914, 'hansi', 2, ' zvczc', ' zxcxzc', 234, ' sdfdsf', '1998-05-10', 'F'),
 (21, 92914, 'hansi', 1, ' zvczc', ' zxcxzc', 234, ' sdfdsf', '1998-05-10', 'F'),
-(22, 92914, 'hansi', 2, ' zvczc', ' zxcxzc', 345, ' sdfdsf', '1998-05-10', 'F'),
+(22, 92914, 'hansi', 1, ' zvczc', ' zxcxzc', 345, ' sdfdsf', '1998-05-10', 'F'),
 (23, 1554, '2626', 0, 'sdfsdf', '23r32r', 23234, 'sdfsf', '1998-03-03', 'm'),
-(24, 1554, '2626', 0, 'sdfsdf', '23r32r', 23234, 'sdfsf', '1998-03-03', 'm'),
 (26, 234, 'dilip', 0, '123', ' dilip123', 7156789, 'makiligasveva', '1998-04-23', 'M'),
-(27, 82934, 'kjkljlkj', 2, 'kjjljkj', '899899', 89898989, '89898', '1982-02-02', 'm');
+(27, 82934, 'kjkljlkj', 2, 'kjjljkj', '899899', 89898989, '89898', '1982-02-02', 'm'),
+(28, 56981, 'A.S.Perera', 2, 'abc123@gmail.com', 'Abc123', 98123454, 'galle,colombo', '1987-09-02', 'f');
 
 --
 -- Indexes for dumped tables
@@ -140,6 +167,12 @@ ALTER TABLE `admin`
 -- Indexes for table `course`
 --
 ALTER TABLE `course`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `exam`
+--
+ALTER TABLE `exam`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -162,19 +195,25 @@ ALTER TABLE `student`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `exam`
+--
+ALTER TABLE `exam`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `spnote`
 --
 ALTER TABLE `spnote`
-  MODIFY `No` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `No` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
